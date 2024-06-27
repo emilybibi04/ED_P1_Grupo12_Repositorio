6df27fe9a549c97f3ad51d52a8ea38ded11d4b0a
Emily Valarezo <emilyvalarezo@RICVAL-M1.local>
2024-06-26 19:50:25 -0500

    Interfaces extras

---
+++ src/main/java/com/espol/ed_p1_grupo12/MenuController.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.espol.ed_p1_grupo12;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class MenuController implements Initializable {
+    
+    @FXML
+    Button boton_info;
+    
+    @FXML
+    Button boton_vehi;
+    
+    @FXML
+    Button boton_fav;
+    
+    @FXML
+    Button boton_cal;
+    
+    @FXML
+    Button boton_ajus;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
